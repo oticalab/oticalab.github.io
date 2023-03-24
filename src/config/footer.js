0ffe394bf92bb8f145cbca2c6a0cac58c94a137b
Bruno Paulino <bruno.paulino@n26.com>
2023-03-24 22:57:34 +0100

    Adição de links

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,45 +1,50 @@
 export const footerSocials = [
   {
-    name: "Instagram",
-    url: "https://www.instagram.com/laboticajp/?hl=de",
-    icon: "mdi:instagram",
+    name: 'Instagram',
+    url: 'https://www.instagram.com/laboticajp/?hl=de',
+    icon: 'mdi:instagram',
   },
   {
-    name: "WhatsApp",
-    url: "https://wa.me/5583982258089?text=Ol%C3%A1%20Lab%20%C3%93tica,%20gostaria%20de%20te%20fazer%20uma%20pergunta!",
-    icon: "mdi:whatsapp",
+    name: 'WhatsApp',
+    url: 'https://wa.me/5583982258089?text=Ol%C3%A1%20Lab%20%C3%93tica,%20gostaria%20de%20te%20fazer%20uma%20pergunta!',
+    icon: 'mdi:whatsapp',
   },
   {
-    name: "Localização",
-    url: "https://goo.gl/maps/LYcYvvXf5FRLj6tZ8",
-    icon: "mdi:google-maps",
+    name: 'Localização',
+    url: 'https://goo.gl/maps/LYcYvvXf5FRLj6tZ8',
+    icon: 'mdi:google-maps',
   },
   // {
   //   name: "Discord",
   //   url: "https://littlesticks.dev/discord",
   //   icon: "ic:baseline-discord",
   // },
-];
+]
 
 export const footerLists = [
   {
-    title: "Unidades",
+    title: 'Unidades',
     items: [
       {
-        title: "Av. João Câncio da Silva, 435 - Manaíra, João Pessoa - PB",
-        slug: "#",
+        title: 'Av. João Câncio da Silva, 435 - Manaíra, João Pessoa - PB',
+        slug: 'https://www.google.com/maps/place/LAB+%C3%93tica/@-7.1102853,-34.8339695,17z/data=!3m1!4b1!4m6!3m5!1s0x7acdd9608da5de1:0x9e15d0b73e908686!8m2!3d-7.1102853!4d-34.8317808!16s%2Fg%2F11rsg9cbq3?coh=164777&entry=tt&shorturl=1',
       },
       {
-        title: "R. José Rufino, 7 - Altiplano Cabo Branco, João Pessoa - PB",
-        slug: "#",
+        title: 'Fone: (83) 98225-8089',
+        slug: 'tel:083982258089',
       },
       {
-        title: "Fone: (83) 98225-8089",
-        slug: "tel:83982258089",
+        title: 'R. José Rufino, 7 - Altiplano Cabo Branco, João Pessoa - PB',
+        slug: 'https://www.google.com/maps/place/Lab+%C3%93tica+Altiplano/@-7.133039,-34.8312047,17z/data=!3m1!4b1!4m6!3m5!1s0x7acc39f6089b945:0xb0b380025b9f6791!8m2!3d-7.133039!4d-34.829016!16s%2Fg%2F11sx5nds9l?coh=164777&entry=tt&shorturl=1',
+      },
+
+      {
+        title: 'Fone: (83) 99914-5042',
+        slug: 'tel:083999145042',
       },
       {
-        title: "Contato via WhatsApp",
-        slug: "https://wa.me/5583982258089?text=Ol%C3%A1%20Lab%20%C3%93tica,%20gostaria%20de%20te%20fazer%20uma%20pergunta!",
+        title: 'Contato via WhatsApp',
+        slug: 'https://wa.me/5583982258089?text=Ol%C3%A1%20Lab%20%C3%93tica,%20gostaria%20de%20te%20fazer%20uma%20pergunta!',
       },
       // {
       // 	title: 'Landing Page 3',
@@ -48,7 +53,7 @@ export const footerLists = [
     ],
   },
   {
-    title: "",
+    title: '',
     items: [
       // {
       //   title: "About",
@@ -65,19 +70,19 @@ export const footerLists = [
     ],
   },
   {
-    title: "Empresa",
+    title: 'Empresa',
     items: [
       {
-        title: "Sobre",
-        slug: "/empresa/sobre",
+        title: 'Sobre',
+        slug: '/empresa/sobre',
       },
       {
-        title: "Contato",
-        slug: "/empresa/contato",
+        title: 'Contato',
+        slug: '/empresa/contato',
       },
       {
-        title: "Blog",
-        slug: "/blog",
+        title: 'Blog',
+        slug: '/blog',
       },
       // {
       //   title: "Customizing Odyssey",
@@ -85,4 +90,4 @@ export const footerLists = [
       // },
     ],
   },
-];
+]
